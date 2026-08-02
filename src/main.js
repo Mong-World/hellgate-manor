1ca526d09f46f380a64cc68f533a631658fb19be
Mong-World <145160040+Mong-World@users.noreply.github.com>
2026-08-02 20:55:07 +0100

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import "../style.css";
-import { Game } from "./Game.js";
-
-const container = document.getElementById("game-shell");
-const game = new Game(container);
-game.start();
-
-window.addEventListener("beforeunload", () => game.dispose());
